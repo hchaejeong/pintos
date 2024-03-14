653b7d81a8debf8676b187ddf8537f8c69132c44
20210330-12 <hanis@kaist.ac.kr>
2024-03-14 16:09:18 +0000

    확인: compare_priority_func check

--- threads/synch.c
+++ threads/synch.c
@@ -71,9 +71,9 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		//list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_push_back (&sema->waiters, &thread_current ()->elem);
 		// 채정이가 만든 priority 비교 함수 쓰면 됨. 기작은 같으니까
-		list_insert_ordered(&sema->waiters,  &thread_current ()->elem, compare_priority_func, 0);
+		//list_insert_ordered(&sema->waiters,  &thread_current ()->elem, compare_priority_func, 0);
 		thread_block ();
 	}
 	sema->value--;
