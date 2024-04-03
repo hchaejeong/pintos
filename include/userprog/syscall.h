c31c943464b08e910f7c0158dbfe9c10a0fa3225
20210330-12 <hanis@kaist.ac.kr>
2024-04-03 08:51:13 +0000

    syscall function insert

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -6,5 +6,9 @@ void syscall_init (void);
 // 여기서도 함수 선언해줘야 함
 //void syscall_handler (struct intr_frame * UNUSED);
 void halt(void);
+void exit(int status);
+tid_t fork(const char *thread_name, struct intr_frame *f);
+int exec (const char *file);
+int wait(tid_t pid);
 
 #endif /* userprog/syscall.h */
