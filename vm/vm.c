bdfe7f9be47e2f4458626bdc3fc51f55de8be743
hchaejeong <hchaejeong@kaist.ac.kr>
2024-05-01 02:31:11 +0000

    anonymous page

--- vm/vm.c
+++ vm/vm.c
@@ -4,6 +4,9 @@
 #include "vm/vm.h"
 #include "vm/inspect.h"
 #include <hash.h>
+#include "threads/mmu.h"
+
+struct list frame_list;
 
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
@@ -17,7 +20,7 @@ vm_init (void) {
 	register_inspect_intr ();
 	/* DO NOT MODIFY UPPER LINES. */
 	/* TODO: Your code goes here. */
-	//여기서 프레임 테이블을 초기화 시켜줘야한다
+	list_init(&frame_list); // 언제나 새로 선언하면 init 해줘야지~
 }
 
 /* Get the type of the page. This function is useful if you want to know the
@@ -146,11 +149,31 @@ vm_evict_frame (void) {
  * space.*/
 static struct frame *
 vm_get_frame (void) {
-	struct frame *frame = NULL;
+	//struct frame *frame = NULL;
+	struct frame *frame;
 	/* TODO: Fill this function. */
-
+	// gitbook을 보면 이걸 제일 먼저 채우라고 적혀있음
+	// 위 설명을 읽어보면, 
+	/* 1. user pool로부터 새로운 physical page를 얻는다 (palloc_get_page 이용) */
+	frame = (struct frame*)malloc(sizeof(struct frame)); // 먼저 크기 배정
+	frame->kva = palloc_get_page(PAL_USER); // user pool로부터 새로운 page 얻음
+	// 여기서, 만약 이미 빈 페이지였다면 그냥 그대로 return하면 되는데,
+	// 이미 frame이 다 차 있어서 빈 페이지가 더이상 남아있지 않다면 victim을 쫓아내고 빈칸으로 만들어야 함
 	ASSERT (frame != NULL);
 	ASSERT (frame->page == NULL);
+
+	if (frame->kva != NULL) {
+		// 이미 빈 칸이 있어서 그게 배치된 경우
+		// 새로운 frame을 가져왔으니 page를 초기화해주고, frame list에 넣기
+		frame->page = NULL;
+		list_push_back(&frame_list, &frame->elem);
+	} else {
+		// 빈칸이 없어서 배당이 안 된 경우
+		// victim을 evict하고, 새로운 frame으로 비운다
+		// 위에 보니 vm_evict_frame()이라는 함수를 사용해야 하는 것 같음
+		// 근데 gitbook에는 일단 panic("todo")를 사용하라고 적혀있는 것 같은데...
+		PANIC("todo");
+	}
 	return frame;
 }
 
@@ -192,10 +215,19 @@ vm_dealloc_page (struct page *page) {
 /* Claim the page that allocate on VA. */
 bool
 vm_claim_page (void *va UNUSED) {
-	struct page *page = NULL;
+	//struct page *page = NULL;
+	struct page *page;
 	/* TODO: Fill this function */
+	// 얘는 page에 va를 할당하라는 claim을 거는 함수
+	// gitbook 말로는 먼저 page를 get한 다음,
+	// vm_do_claim_page를 하랜다.
+	page = spt_find_page(&thread_current()->spt, va);
+	if (page != NULL) {
+		return vm_do_claim_page (page);
+	} else {
+		return false;
+	}
 
-	return vm_do_claim_page (page);
 }
 
 /* Claim the PAGE and set up the mmu. */
@@ -208,8 +240,18 @@ vm_do_claim_page (struct page *page) {
 	page->frame = frame;
 
 	/* TODO: Insert page table entry to map page's VA to frame's PA. */
+	// gitbook 설명대로라면, 위에서 vm_get_page로 frame을 갖고 온 다음에,
+	// MMU를 setting해준다. 즉, page table 안에서 va와 pa를 mapping하는걸 추가한다.
+	// 그리고, 잘 완료되었으면 true, 아니면 false를 반환한다
+	//bool mapping_va_pa = install_page(page->va, frame->kva, page->write);
+	bool mapping_va_pa = pml4_set_page(&thread_current()->pml4, page->va, frame->kva, page->write);
+	if (mapping_va_pa) {
+		return swap_in (page, frame->kva);
+	} else {
+		return false;
+	}
 
-	return swap_in (page, frame->kva);
+	//return swap_in (page, frame->kva);
 }
 
 /* Initialize new supplemental page table */
