0c47ddb3c6f1ddcada962134f6fae09881e74679
20210330-12 <hanis@kaist.ac.kr>
2024-05-29 11:13:04 +0000

    just save for pull filesys

--- filesys/filesys.c
+++ filesys/filesys.c
@@ -113,21 +113,49 @@ filesys_create (const char *name, off_t initial_size) {
  * or if an internal memory allocation fails. */
 struct file *
 filesys_open (const char *name) {
-	struct thread *curr = thread_current();
-	struct dir *dir;
-	if (curr->current_dir == NULL) {
-		dir = dir_open_root();
-	} else {
-		dir = curr->current_dir;
-	}
-	//struct dir *dir = dir_open_root ();
-	struct inode *inode = NULL;
+	#ifdef FILESYS
+		
+		char *copy_name = (char*)malloc(strlen(name) + 1);
+		strlcpy(copy_name, name, strlen(name) + 1);
 
-	if (dir != NULL)
-		dir_lookup (dir, name, &inode);
-	dir_close (dir);
+		char *final_name = (char*)malloc(strlen(name) + 1);
 
-	return file_open (inode);
+		struct inode *inode = NULL;
+
+		// 여기는 chdir과 같음. 경로 초기 세팅
+		struct dir *dir = dir_open_root(); // 일단 기본이 되는 root 경로로 열어두고 시작
+		if (copy_name[0] != '/') {
+			dir = dir_reopen(thread_current()->current_dir);
+		}
+		dir = parsing(dir, copy_name, final_name);
+		if (dir != NULL) {
+			dir_lookup(dir, final_name, &inode);
+		}
+		free(copy_name);
+		free(final_name);
+		dir_close(dir);
+		
+		return file_open(inode);
+
+	#else
+		struct thread *curr = thread_current();
+		struct dir *dir;
+		/*
+		if (curr->current_dir == NULL) {
+			dir = dir_open_root();
+		} else {
+			dir = curr->current_dir;
+		}
+		*/
+		struct dir *dir = dir_open_root ();
+		struct inode *inode = NULL;
+
+		if (dir != NULL)
+			dir_lookup (dir, name, &inode);
+		dir_close (dir);
+
+		return file_open (inode);
+	#endif
 }
 
 /* Deletes the file named NAME.
@@ -167,4 +195,71 @@ do_format (void) {
 #endif
 
 	printf ("done.\n");
+}
+
+// syscall.c의 mkdir이랑 symlink에서 썼던 parsing하는게 계속 쓰이네 여기서도
+struct dir *parsing(struct dir *dir, char *path, char *final_name) {
+	if (path == NULL || strlen(path) == 0) {
+		return NULL;
+	}
+
+	char *save;
+	char *path_token = strtok_r(path, "/", &save);
+	struct inode *inode = NULL;
+
+	if (path_token == NULL) {
+		strlcpy(final_name, ".", 2); // "/"인 경우에는, file_name은 .이 되어야 함
+	}
+	
+	// 여기서의 목적은 file_name만 parsing 해내는 것!
+	while (path_token != NULL) {
+		bool success_lookup = dir_lookup(dir, path_token, &inode);
+		bool is_inode_dir = inode_is_directory(inode);
+		if (!(success_lookup && is_inode_dir)) {
+			dir_close(dir);
+			inode_close(inode);
+			dir = NULL; // dir이 없는 거니까 dir은 null로 세팅해줘야
+			break;
+		} else {
+			// 링크 파일인 경우는 앞서서 한 번 더 돌리면 될 것 같음
+			if (check_symlink(inode)) {
+				// 아니 대체 왜 inode->data.symlink가 안되는건데 ㅋㅋ
+				// inode의 path를 복사해온다!
+				char *inode_path = (char*)malloc(sizeof(length_symlink_path(inode)));
+				strlcpy(inode_path, inode_data_symlink_path(inode), length_symlink_path(inode));
+				
+				// inode path 경로에 symlink 뒷부분을 갖다붙이면 됨
+				// ../file 이런 식으로 되어있던 걸 inode path/file 이런 형식으로!
+				strlcat(inode_path, "/", strlen(inode_path) + 2);
+				strlcat(inode_path, save, strlen(inode_path) + strlen(save) + 1);
+
+				dir_close(dir);
+
+				// 그리고 while문 다시 시작해야함!
+				dir = dir_open_root();
+				if (path[0] != '/') {
+					dir = dir_reopen(thread_current()->current_dir);
+				}
+				strlcpy(path, inode_path, strlen(inode_path) + 1);
+				free(inode_path);
+				path_token = strtok_r(path, "/", &save);
+				continue;
+			}
+
+
+			dir_close(dir);
+			dir = dir_open(inode);
+			// 근데 여기서, 경로의 마지막은 file의 이름이므로 그걸 저장해야함
+			char *check_next = strtok_r(NULL, "/", &save);
+			if (check_next == NULL) {
+				// file_name에 file name 복사해두기!
+				strlcpy(final_name, path_token, strlen(path_token) + 1);
+				break;
+			} else {
+				path_token = final_name;
+			}
+		}
+	}
+
+	return dir;
 }
